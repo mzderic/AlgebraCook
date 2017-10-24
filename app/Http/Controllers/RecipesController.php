9b7b7dc8bea7c45faa9373d8de63c6ece45bd77d
Maja Žderić <majslcr@yahoo.com>
2017-10-24 20:59:06 +0200

    napravljen RecipesController

---
+++ app/Http/Controllers/RecipesController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class RecipesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ("Popis svih recepata");
+    }
+
+    
+    public function add()
+    {
+        return ("Prikaz view-a sa web obrascem za unos.");
+    }
+	
+	public function save(Request $request) 
+	{
+		// $request->nameInputa = vrijednostInputa
+		return("Spremam podatke iz webobrasca u bazu");
+	}
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function view($id)
+    {
+        return "Prikaz recepta ID: " . $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return "Promjena recepta sa ID:" . $id; // Prikaz web obrasca
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "Spremanje promjena recepta ID: " . $id;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        return "Brisanje recepta ID: " . $id;
+    }
+}
