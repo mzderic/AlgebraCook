63c75d8ea5f3bc5c9b9f01c1f350662981b5cff5
Maja Žderić <majslcr@yahoo.com>
2017-10-24 20:57:40 +0200

    napravljene route za RecipesController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/recipes', 'RecipesController@index'); //prikaz scih recepata(popis)
+
+Route::get('/recipes/add', 'RecipesController@add'); //prikaz view-a za web obrascem za unos
+Route::post('/recipes/add', 'RecipesController@save'); // spremanje podataka u bazu
+
+Route::get('/recipes/view/{id}', 'RecipesController@view'); // detaljan prikaz određenog recepta po id-u
+
+Route::get('/recipes/edit/{id}', 'RecipesController@edit'); // prikaz webobrasca za uređivanje
+Route::post('/recipes/edit', 'RecipesController@update'); // ažuriranje podataka u bazi
+
+
