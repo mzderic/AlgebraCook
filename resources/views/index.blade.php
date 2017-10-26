3ec110f2f8cb6a71dfd1dbcfe8774ad6db548956
Maja Žderić <majslcr@yahoo.com>
2017-10-26 19:44:43 +0200

    master,index i errors view dodani

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+@extends('master')
+
+@section('title', 'Recepti')
+
+@section('content')
+<div class="container">
+	<div class="col-md-10 col-md-offset-1">
+		<div class="panel panel-default">
+			<div class="panel-heading">Recepti<br><a href="{{url('/recipes/add')}}">
+				<i class="fa fa-btn fa-plus"></i>
+				Dodaj novi recept
+				</a>
+			</div>
+			<div class="panel-body">
+				<ul class="list-group">
+					@foreach ($recipes as $recipe)
+					<li class="list-group-item">
+						<a href="resipes/view/{{ $recipe->id }}">{{ $recipe->name }}</a>
+					</li>
+					@endforeach
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
